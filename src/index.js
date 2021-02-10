aa614e2c37d69a66bd5728e21743335cd732eb90
Sam Hall <mann.samantha11@gmail.com>
2021-02-10 10:44:48 -0500

    spice title

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const ingredientUrl = "http://localhost:3000/ingredients"
 
 const spiceImages = document.getElementById("spice-images")
 const spiceDetail = document.getElementById("spice-blend-detail")
+const spiceDetailImage = document.querySelector(".detail-image")
+const spiceTitle = document.querySelector(".title")
 const spiceIngredients = document.getElementById("ingredients-container")
-const ingredientsUl = document.getElementById("ingredients-list")
+const ingredientsUl = document.querySelector(".ingredients-list")
 
 const updateForm = document.getElementById("update-form")
 const newIngredientForm = document.getElementById("ingredient-form")
@@ -25,24 +27,26 @@ function renderSpice(spiceObj){
     const spiceImage = document.createElement("img")
         spiceImage.src = spiceObj.image
         spiceImage.alt = spiceObj.title
-    const spiceTitle = document.createElement("h2")
-        spiceTitle.textContent = spiceObj.title
-        spiceTitle.dataset.id =spiceObj.id
-  
+        spiceImage.dataset.id = spiceObj.id
+    spiceTitle.innerHTML = spiceObj.title
+        spiceTitle.dataset.id = spiceObj.id
+    
+
     spiceImages.append(spiceImage)
     spiceDetail.append(spiceTitle)  
+    // spiceDetailImage.append(spiceImage)
 }
 
 function renderIngredient(ingredientObj){
     const ingredientLi = document.createElement("li")
         ingredientLi.classList.add("ingredient-li")
-        ingredientLi.dataset.id = ingredientObj.id
+        ingredientLi.dataset.id = spiceObj.id
     const ingredientName = document.createElement("h5")
         ingredientName.textContent = ingredientObj.name
+        ingredientName.dataset.id = spiceObj.id
 
     ingredientLi.append(ingredientName)
     ingredientsUl.append(ingredientLi)
-
 }
 
 fetchSpices()
